63b728d886a91e9c3c92619bce2e049db2a40da4
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-10-14 12:08:50 +0100

    Add input for date

--- src/components/CountdownComponent.vue
+++ src/components/CountdownComponent.vue
@@ -1,16 +1,25 @@
 <template>
-  <div>
-    <h1>{{ heading }}</h1>
+  <div id="countdown">
+    <h1>{{ pageHeading }}</h1>
+
+    <h2>{{ enterDateHeading }}</h2>
+    <input type="date" v-model="date" />
+    <h3>{{ date }}</h3>
 
     <vue-countdown
       :time="54 * 24 * 60 * 60 * 1000"
       v-slot="{ days, hours, minutes, seconds }"
     >
-      {{ days }} dagen, {{ hours }} huren, {{ minutes }} minuten en {{ seconds }} seconden
-    </vue-countdown> 
-<br>
-<br>
-    <img src="https://images0.persgroep.net/rcs/FzPSQAAzi0m-mCgUXcImyDbPUSQ/diocontent/160661570/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8" alt="Sinterklaas in Leiden, 2019" style="display: block; margin-left: auto; margin-right: auto; width: 50%;">
+      {{ days }} dagen, {{ hours }} huren, {{ minutes }} minuten en
+      {{ seconds }} seconden
+    </vue-countdown>
+    <br />
+    <br />
+    <img
+      src="https://images0.persgroep.net/rcs/FzPSQAAzi0m-mCgUXcImyDbPUSQ/diocontent/160661570/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8"
+      alt="Sinterklaas in Leiden, 2019"
+      style="display: block; margin-left: auto; margin-right: auto; width: 50%"
+    />
   </div>
 </template>
 
@@ -24,7 +33,9 @@ export default {
   },
   data() {
     return {
-      heading: "Hoeveel dagen tot Sinterklaas?",
+      pageHeading: "Hoeveel dagen tot Sinterklaas?",
+      enterDateHeading: 'Kies een datum aub:',
+      date: ''
     };
   },
 };
